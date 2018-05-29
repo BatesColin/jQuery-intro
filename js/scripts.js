91a43f2ae2a9b4891d17d861d1173fea957739bd
Alex Bunnell and Colin Bates <alex@>
2018-05-29 14:06:02 -0700

    Add initial HTML, CSS and JS to practice using Jquery.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+});
+
+$("p").click(function() {
+    $("img").show();
+  });
+});
